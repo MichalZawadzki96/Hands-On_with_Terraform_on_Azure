48b68fa3307d8c8376f11f910ff8f61cb45eded4
MichalZawadzki96 <michal.a.zawadzki@accenture.com>
2025-10-01 13:40:16 +0000

    res group

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.43.0"
+    }
+  }
+  cloud {
+
+    organization = "ZawaTF"
+
+    workspaces {
+      name = "Hands-On_With_Terraform_On_Azure"
+    }
+  }
+
+}
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  subscription_id = "9734ed68-621d-47ed-babd-269110dbacb1"
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "813-fef8349a-hands-on-with-terraform-on-azure"
+  location = "westus"
+}
+
+module "securestorage" {
+  source               = "app.terraform.io/ZawaTF/securestorage/azurerm"
+  version              = "1.0.0"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_name = "mzawstgacct011025"
+}
